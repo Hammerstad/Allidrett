0e646c0c88c2a503a2d93c68ddaaf9def3b32528
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2020-08-27 22:27:08 +0200

    Added wildcard to allowed hosts

--- allidrett/settings.py
+++ allidrett/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lt*iuhi&z9a$is0*ojma0n!kg6sj4p@!!nklg#d^c_p@!u*+4h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'allidrett.app'
 ]
 
 MIDDLEWARE = [
