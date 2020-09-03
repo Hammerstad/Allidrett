311b23044ce2e7d33baddafb313f69ceca0ea376
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2020-09-03 22:37:25 +0200

    Added CC to mails sent

--- allidrett/app/views.py
+++ allidrett/app/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, redirect
 from django.shortcuts import HttpResponseRedirect
 from .forms import RegistrationForm
 from .models import Registration
-from django.core.mail import send_mail
 from django.conf import settings
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -28,15 +28,17 @@ def on_success(request, registration_id):
     registration = Registration.objects.get(id=registration_id)
 
     if settings.DEBUG:
-        subject = 'Allidrett Nidelv IL Høst 2020 Registrering'
-        message = """
-        Takk for at du har meldt barnet ditt %s på allidrett i regi av Nidelv Idrettslag. 
-        Dere er påmeldt partiet som går hver %s fra %s til %s. 
-         
-        Vi gleder oss til å treffe dere.
-        """ % (registration.name, registration.party.day, registration.party.start_time, registration.party.end_time)
-        email_from = settings.EMAIL_HOST_USER
-        recipient_list = [registration.email,]
-        send_mail( subject, message, email_from, recipient_list )
+        email = EmailMessage(
+            'Allidrett Nidelv IL Høst 2020 Registrering',
+            """Takk for at du har meldt barnet ditt %s på allidrett i regi av Nidelv Idrettslag. 
+            Dere er påmeldt partiet som går hver %s fra %s til %s. 
+            
+            Vi gleder oss til å treffe dere.
+            """ % (registration.name, registration.party.day, registration.party.start_time, registration.party.end_time),
+            settings.EMAIL_HOST_USER,
+            [registration.email, ],
+            cc=['allidrett@nidelv-il.no', ],
+        )
+        email.send()
 
     return render(request, 'registration_receipt.html', {'registration': registration})
