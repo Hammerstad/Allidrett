518d4f2e34508c7f75075080d49b3aae1a5a552a
Eirik Mildestveit Hammerstad <emhammerstad@gmail.com>
2020-09-04 22:59:14 +0200

    Added admin listing of registrations

--- allidrett/app/admin.py
+++ allidrett/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Registration
 
-# Register your models here.
+@admin.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ('email','name','get_party_id','year_of_birth')
+
+    def get_party_id(self, obj):
+        return "%s %s-%s" % (obj.party.day, obj.party.start_time, obj.party.end_time)
+    get_party_id.short_description = 'party'
+    get_party_id.admin_order_field = 'party__id'
